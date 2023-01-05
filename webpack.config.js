bcd7326261135d3e7ab4f89375444a99f770b1de
Jackie Nguyen <jackquelineanguyen@gmail.com>
2023-01-04 17:33:03 -0800

    moved index.html to root file

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, '/src/index.html'),
+      template: path.join(__dirname, 'index.html'),
     }),
   ],
   module: {
